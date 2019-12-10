ab20f5df904ade201d220fa9f31bf0483afc2bc8
Ajit Jain <ajit-jain@Ajits-MacBook-Air.local>
2019-12-10 21:44:38 +0530

    message exchange service added

--- server.js
+++ server.js
@@ -7,53 +7,15 @@ const bluebird  = require('bluebird');
 const _ = require('lodash');
 const async = require('async');
 const net = require('net');
-const email = "ajit.x@venturepact.com";
+const email_verifier = require('./lib/email-verifier');
+
 const dnscache = require('dnscache')({
         "enable" : true,
-        "ttl" : 300,
-        "cachesize" : 1000
-    });
-    const dnsPromises = bluebird.promisify(dnscache.resolveMx);
+        "ttl" : 24*60*60,
+        "cachesize" : 10000
+});
+const dnsPromises = bluebird.promisify(dnscache.resolveMx);
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:false}));
 
-// app.get('/',(req,res,next)=>{
-    bluebird.coroutine(function*(){
-    let MxRecords = yield dnsPromises('venturepact.com');
-    MxRecords = _.sortBy(MxRecords,function(item){
-      return item.priority;
-    });
-    console.log(MxRecords);
-    async.detect(MxRecords,(record,callback)=>{
-      //console.log("DD",checkEmailByMessage(record));
-          let result =checkEmailByMessage(record);
-          result.then(status=>callback(null,true));
-    },(err,r)=>{
-        console.log(r);
-    })
-    
-  }).apply(this).catch((err)=>{
-    console.log(err);
-  });
-
-function checkEmailByMessage(record){
-    return new Promise((resolve,reject)=>{
-          console.log(record['exchange']);
-          setupConnection(record);
-    });
-
-}
-function setupConnection(record){
-  let client = net.createConnection(25,record['exchange'],()=>{
-            console.log("Connected");
-          });  
-          client.on('error',(error)=>{
-            console.log("error");
-            client.emit('end');
-          });
-          client.on('data',(data)=>{
-            console.log("Received data",data);
-            resolve(true);
-          });
-}
 app.listen(8080,'localhost',()=>console.log("Server Connected....."));
